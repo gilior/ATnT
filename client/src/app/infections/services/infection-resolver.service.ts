bda22cee316d2ef67e3c9306885cc97e646b7281
lior gishry <liorgish@gmail.com>
2020-07-09 19:06:34 +0300

    more

---
+++ client/src/app/infections/services/infection-resolver.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {InfectionsService} from './infections.service';
+import {ActivatedRouteSnapshot, Router, RouterStateSnapshot} from '@angular/router';
+import {Observable, of} from 'rxjs';
+import {IInfection} from '../../common/model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfectionResolverService {
+  constructor(private infectionsService: InfectionsService, private router: Router) {
+  }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<IInfection> | Observable<never> {
+    let id = +route.paramMap.get('id');
+
+    let infection = this.infectionsService.getInfection(id);
+    return of(infection);
+  }
+}
